d80719e916bfbb0e054dfaf134b91718a04b79a5
sulenq <whyimustwriteit@gmail.com>
2023-10-29 21:27:29 +0700

    tes

--- src/middleware/AuthJwt.tsx
+++ src/middleware/AuthJwt.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import FullPageLogo from "../components/FullPageLogo";
+// import React, { useState } from "react";
+// import FullPageLogo from "../components/FullPageLogo";
 
 type Props = { children: JSX.Element };
 
 export default function AuthJwt({ children }: Props) {
-  const [page, setPage] = useState(<FullPageLogo />);
+  // const [page, setPage] = useState(<FullPageLogo />);
 
   //TODO chek is JWT exist
   // setPage(children);
